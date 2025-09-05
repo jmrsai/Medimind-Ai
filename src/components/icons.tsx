033cafe6b697803a6d8d4755998b234c15229b88
Jmr Sai <jmrsai987@gmail.com>
2025-09-05 18:06:56 +0000

    Icons.tsx build Icon as a medical professional ai

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,24 +3,25 @@ import type { SVGProps } from "react";
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
     >
-      <path d="M12 2a10 10 0 1 0 10 10" />
-      <path d="m12 12 4 4" />
-      <path d="M12 12V2" />
-      <path d="m12 12-4 4" />
-      <path d="M12 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-      <path d="M7 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
-      <path d="M17 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
+        <path d="M12 2a10 10 0 1 0 10 10" />
+        <path d="M12 12v-2a2 2 0 0 1 2-2h2" />
+        <path d="M12 12h-2a2 2 0 0 0-2 2v2" />
+        <path d="M12 12a2 2 0 0 1-2-2V8" />
+        <path d="M12 12a2 2 0 0 0 2 2h2" />
+        <path d="M12 8a2 2 0 0 0-2-2H8" />
+        <path d="M12 16a2 2 0 0 1 2 2v2" />
+        <path d="M10 12a2 2 0 0 0-2-2H6" />
+        <path d="M14 12a2 2 0 0 1 2 2v2" />
+        <circle cx="12" cy="12" r="2" />
     </svg>
   );
 }
@@ -39,17 +40,13 @@ export function OldLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M9.5 2.5a2.5 2.5 0 0 1 5 0" />
-      <path d="M12 12a5 5 0 0 0 5-5" />
-      <path d="M7 7a5 5 0 0 0 5 5" />
-      <path d="M12 22a4 4 0 0 0 4-4" />
-      <path d="M8 18a4 4 0 0 0 4 4" />
-      <path d="M12 2a2.5 2.5 0 0 0-2.5 2.5" />
-      <path d="M14.5 4.5a2.5 2.5 0 0 0-2.5-2.5" />
-      <path d="M12 12a5 5 0 0 1-5 5" />
-      <path d="M17 12a5 5 0 0 1-5-5" />
-      <path d="M12 22a4 4 0 0 1-4-4" />
-      <path d="M8 18a4 4 0 0 1 4 4" />
+      <path d="M12 2a10 10 0 1 0 10 10" />
+      <path d="m12 12 4 4" />
+      <path d="M12 12V2" />
+      <path d="m12 12-4 4" />
+      <path d="M12 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+      <path d="M7 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
+      <path d="M17 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
     </svg>
   );
 }
